645ca9ed3e7b26bacdff906f3d9e269111a53d20
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2018-01-28 00:18:56 +0000

    update data processing script

--- data-raw/process_data.R
+++ data-raw/process_data.R
@@ -7,5 +7,5 @@
 studentData <- read.csv("data-raw/studentData.csv")
 devtools::use_data(studentData, overwrite = TRUE)
 
-studentPlacements <- read.csv("data-raw/studentPlacements.csv")
-devtools::use_data(studentPlacements, overwrite = TRUE)
+studentPlacement <- read.csv("data-raw/studentPlacements.csv")
+devtools::use_data(studentPlacement, overwrite = TRUE)
