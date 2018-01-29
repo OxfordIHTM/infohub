6f9cf8c0a22c08b36c1151b637226bdbad7a0dd0
ernestguevarra <ernestgmd@gmail.com>
2018-01-29 06:20:14 +0000

    debug openHub

--- R/openHub.R
+++ R/openHub.R
@@ -7,7 +7,7 @@
 #' @return NULL
 #' @export
 #'
-openHub <- function(data) {
+openHub <- function() {
   #
   # Server
   #
@@ -17,33 +17,104 @@ openHub <- function(data) {
     #
     output$map <- renderLeaflet({
       leaflet() %>%
+        #
+        # Add base map tiles - Mapbox Standard
+        #
         addTiles(
-          urlTemplate = "https://api.mapbox.com/styles/v1/ernestguevarra/cj5di36jn0gxg2rphjn3yetpt/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXJuZXN0Z3VldmFycmEiLCJhIjoiejRRLXlZdyJ9.sqS1zi0rDH5CIzvcn9SXSg",
-          attribution = "Map by <a href = 'http://www.mapbox.com/'>Mapbox</a>"
+          urlTemplate = mapStyle$url[mapStyle$style == "mapbox.standard"],
+          attribution = "Map by <a href = 'http://www.mapbox.com/'>Mapbox</a>",
+          group = "Mapbox Standard"
         ) %>%
+        #
+        # Add base map tiles - Mapbox Satellite
+        #
+        addTiles(
+          urlTemplate = mapStyle$url[mapStyle$style == "mapbox.satellite"],
+          attribution = "Map by <a href = 'http://www.mapbox.com/'>Mapbox</a>",
+          group = "Mapbox Satellite"
+        ) %>%
+        #
+        # Add base map tiles - Mapbox Street
+        #
+        addTiles(
+          urlTemplate = mapStyle$url[mapStyle$style == "mapbox.street"],
+          attribution = "Map by <a href = 'http://www.mapbox.com/'>Mapbox</a>",
+          group = "Mapbox Street"
+        ) %>%
+        #
+        # Add markers for student origin location
+        #
+        addCircleMarkers(data = studentData,
+                         ~lon, ~lat,
+                         radius = 4 * studentData$n,
+                         stroke = FALSE,
+                         fillOpacity = 0.8,
+                         fillColor = "#002147",
+                         popup = paste("<b>Country:</b> ", studentData$country, "<br/>",
+                                       "<b>No. of students from country:</b> ", studentData$n, " student/s", sep = ""),
+                         group = "Student Origin Location") %>%
+        #
+        # Add markers for student placements
+        #
+        addMarkers(data = studentPlacement,
+                   ~lon, ~lat,
+                   popup = paste("<b>Location:</b> ", studentPlacement$location, ", ", studentPlacement$country, "<br/>",
+                                 "<b>Organisation:</b> ", studentPlacement$org_full, " - ", studentPlacement$org_short, "<br/>",
+                                 "<b>Subject:</b> ", studentPlacement$subject, "<br/>",
+                                 "<a href='", studentPlacement$url, "'>More Info</a>", sep = ""),
+                   clusterOptions = markerClusterOptions(),
+                   group = "Student Placement") %>%
+        #
+        # Add layers control
+        #
+        addLayersControl(
+          baseGroups = c("Mapbox Standard", "Mapbox Satelllite", "Mapbox Street"),
+          overlayGroups = c("Student Origin Location", "Student Placement"),
+          options = layersControlOptions(collapsed = FALSE)
+        ) %>%
+        #
+        # Add mini-map
+        #
+        addMiniMap(
+          tiles = providers$Esri.NatGeoWorldMap,
+          toggleDisplay = TRUE) %>%
+        #
+        # Set view
+        #
         setView(lng = 20, lat = 20, zoom = 3)
     })
-    #
-    #
-    #
-    leafletProxy("map", data = data) %>%
-      clearShapes() %>%
-      addCircleMarkers(~lon, ~lat, radius = data$n * 4,
-        stroke = FALSE, fillOpacity = 0.6, fillColor = "#002147")
   }
   #
   # UI
   #
-  ui <- navbarPage(title = "Oxford IHTM Infohub", id = "chosenTab",
+  ui <- navbarPage(title = "Oxford University MSc in International Health and Tropical Medicine", id = "chosenTab",
     tabPanel(title = "", value = 1, icon = icon(name = "home", class = "fa-lg"),
-      div(class = "outer", tags$head(includeCSS(system.file("shiny-server", "styles.css", package = "infohub"))),
-        leafletOutput("map", width = "100%", height = "100%")
+      div(class = "outer",
+        tags$head(
+          includeCSS(system.file("shiny-server", "styles.css", package = "infohub")),
+          tags$link(rel = "icon", href = "ox-square-border.gif")
+        ),
+        leafletOutput("map", width = "100%", height = "100%"),
+        #
+        # Create absolute panel - INPUT
+        #
+        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                      draggable = FALSE, top = "auto", left = 10, right = "auto", bottom = 10,
+                      width = 330, height = "auto",
+          #
+          # Panel section header
+          #
+          div(style="display: inline-block;vertical-align:middle;",
+            h4("MSc in International Health and Tropical Medicine"),
+            h6("The MSc in International Health and Tropical Medicine is a full-time
+            one-year multidisciplinary and interdisciplinary programme examining
+            major challenges to the health of populations in resource-limited
+            contexts. The course is embedded within the Oxford Centre for Tropical
+            Medicine and Global Health, Nuffield Department of Medicine.")
+          )
+        )
       )
-    ),
-    #
-    #
-    #
-    tabPanel(title = "Settings", value = 2, icon = icon(name = "cog", class = "fa-lg"))
+    )
   )
   #
   #
